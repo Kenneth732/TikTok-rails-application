0db4fa1d217dc18b013eb95bf054e2b0b90af786
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-19 16:34:30 +0300

    data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,14 @@
 # Assuming you have a model named "Video"
 Video.create([
     {
-
+        title: "The Color Purple",
+        year: 2020,
+        length: 154,
+        director: "Drummatic Class",
+        description: "Whoopi Goldberg brings Alice Walker's Pulitzer Prize-winning feminist novel to life as Celie, a Southern woman who suffered abuse over decades. A project brought to a hesitant Steven Spielberg by producer Quincy Jones, the film marks Spielberg's first female lead.",
+        movie_file: "./movies/p1.mp4",
+        category: "Drama",
+        discount: false,
+        female_director: false
     }
 ])
