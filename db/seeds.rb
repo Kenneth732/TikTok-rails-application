1d2f1130605a31fb6e12ccff5b54222f82b55a6d
Kenneth <kenneth.mburu@student.moringaschool.com>
2023-07-19 16:34:58 +0300

    data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,5 +10,15 @@ Video.create([
         category: "Drama",
         discount: false,
         female_director: false
+    },
+    {
+        year: 2002,
+        length: 123,
+        director: "Indian Lough",
+        description: "Her portrait, with that thick unibrow and un-waxed upper lip, has become an iconic symbol of feminism. Julie Taymor's biopic takes us behind the canvas to reveal the artist, the activist, the revolutionary. And knowing what we do now about lead actress Selma Hayek's off-screen experience, this film proves an even greater victory.",
+        movie_file: "./movies/p2.mp4",
+        category: "Drama",
+        discount: false,
+        female_director: true
     }
 ])
